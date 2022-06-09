53cf219524732ff6d98ba18a736de210c497eb5b
Jibitesh Jena <102899202+jibitesh2112@users.noreply.github.com>
2022-06-09 13:45:53 +0530

    Add files via upload

---
+++ Question/maximum for each contiguous  .c
@@ -0,0 +1,29 @@
+
+#include<stdio.h> 
+  
+void printKMax(int arr[], int n, int k) 
+{ 
+    int j, max; 
+  
+    for (int i = 0; i <= n-k; i++) 
+    { 
+        max = arr[i]; 
+  
+        for (j = 1; j < k; j++) 
+        { 
+            if (arr[i+j] > max) 
+               max = arr[i+j]; 
+        } 
+        printf("%d ", max); 
+    } 
+} 
+  
+  
+int main() 
+{ 
+    int arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    int k = 3; 
+    printKMax(arr, n, k); 
+    return 0; 
+}
