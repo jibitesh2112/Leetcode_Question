a8e70c8287139dd428f0437a4b2758f7a2f1f573
Jibitesh Jena <102899202+jibitesh2112@users.noreply.github.com>
2022-06-10 13:16:05 +0530

    Add files via upload

---
+++ Question/Percentage of Letter in String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+        double count=0,tot=0;
+       for(int i=0;i<s.length();i++)
+       {
+           if(s.charAt(i)==letter)
+               count++;
+           tot++;
+        }
+        return (int)((count/tot)*100);
+    }
+}
